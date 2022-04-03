e7d88296631252570b2340ca74e5cee55ce5fa12
Peter Marynczak <piotr.marynczak.programista@gmail.com>
2022-04-03 15:31:22 -0400

    added 3 cards with icons and descriptions

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,44 @@
+import React, { Component, Fragment } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../../asset/css/custom.css";
+import "../../asset/css/bootstrap.min.css";
+
+import designIcon from "../../asset/image/design.png";
+import ecommerceIcon from "../../asset/image/ecommerce.png";
+import webIcon from "../../asset/image/web.png";
+
+class Services extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container>
+          <Row>
+            <Col lg={4} md={6} sm={12}>
+              <div className="serviceCard text-center">
+                <img className="ecommerceIcon" src={ecommerceIcon} />
+                <h2 className="serviceName">E-commerce</h2>
+                <p className="serviceDescription">I will design and develop e-commerce online store website</p>
+              </div>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <div className="serviceCard text-center">
+                <img className="designIcon" src={designIcon} />
+                <h2 className="serviceName">Web Design</h2>
+                <p className="serviceDescription">Qualified web design and attractive effected which catches visitor's eye</p>
+              </div>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <div className="serviceCard text-center">
+                <img className="webIcon" src={webIcon} />
+                <h2 className="serviceName">Web Development</h2>
+                <p className="serviceDescription">Clean and fresh issue free code to make your website dynamic perfectly</p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default Services;
